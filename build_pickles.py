bb026086f933656cc9547f81e5efd20eb75ef6c9
Ba Son <bagiangson2001@gmail.com>
2022-12-22 14:38:29 +0700

    change tokenizer

--- build_pickles.py
+++ build_pickles.py
@@ -7,48 +7,17 @@ from pathlib import Path
 from utils import convert_to_dataset
 
 from torchtext.legacy.data import Field
-from soynlp.word import WordExtractor
-from soynlp.tokenizer import LTokenizer
-
-
-def build_tokenizer():
-    """
-    Train soynlp tokenizer which will be used to tokenize Korean input sentence
-    """
-    print(f'Now building soy-nlp tokenizer . . .')
-
-    data_dir = Path().cwd() / 'data'
-    train_file = os.path.join(data_dir, 'corpus.csv')
-
-    df = pd.read_csv(train_file, encoding='utf-8')
-
-    # if encounters non-text row, we should skip it
-    kor_lines = [row.korean
-                 for _, row in df.iterrows() if type(row.korean) == str]
-
-    word_extractor = WordExtractor(min_frequency=5)
-    word_extractor.train(kor_lines)
-
-    word_scores = word_extractor.extract()
-    cohesion_scores = {word: score.cohesion_forward
-                       for word, score in word_scores.items()}
-
-    with open('pickles/tokenizer.pickle', 'wb') as pickle_out:
-        pickle.dump(cohesion_scores, pickle_out)
-
-
+from tokenize import Tokenizer
+tokenizer =  Tokenizer()
 def build_vocab(config):
     """
     Build vocab used to convert input sentence into word indices using soynlp and spacy tokenizer
     Args:
         config: configuration containing various options
     """
-    pickle_tokenizer = open('pickles/tokenizer.pickle', 'rb')
-    cohesion_scores = pickle.load(pickle_tokenizer)
-    tokenizer = LTokenizer(scores=cohesion_scores)
 
     # include lengths of the source sentences to use pack pad sequence
-    kor = Field(tokenize=tokenizer.tokenize,
+    kor = Field(tokenize=tokenizer.tokenize_kr,
                     lower=True,
                     batch_first=True)
 
---
+++ tokenizer.py
@@ -0,0 +1,16 @@
+from underthesea import word_tokenize
+import spacy
+
+class Tokenizer:
+    def __init__(self):
+        self.spacy_jp = spacy.load('ja_core_news_sm')
+        self.spacy_kr = spacy.load('ko_core_news_sm')
+
+    def tokenize_vi(self,text):
+        return word_tokenize(text,format='')
+
+    def tokenize_jp(self, text):
+        return [tok.text for tok in self.spacy_jp.tokenizer(text)]
+
+    def tokenize_kr(self, text):
+        return [tok.text for tok in self.spacy_kr.tokenizer(text)]
